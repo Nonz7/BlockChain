e10ebe565bd2531cc53211fa4c7e96702efbe72f
Nonz7 <nonscbza555@gmail.com>
2024-03-30 20:24:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css';
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Vote from './components/pages/Vote';
+import SignUp from './components/pages/SignUp'
+
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/vote' component={Vote} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
